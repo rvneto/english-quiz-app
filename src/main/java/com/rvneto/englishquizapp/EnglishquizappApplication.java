306a156361fe38af67ca749e3ff98c97f487a1cf
Roberto de Vargas <rvneto.rs@gmail.com>
2025-10-06 21:27:12 -0300

    Initial commit

---
+++ src/main/java/com/rvneto/englishquizapp/EnglishquizappApplication.java
@@ -0,0 +1,13 @@
+package com.rvneto.englishquizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishquizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishquizappApplication.class, args);
+	}
+
+}
